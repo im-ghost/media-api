54d32707135334786361eabd63cf9c7d931d8044
Ghost <culestfrosh@gmail.com>
2023-03-18 14:59:20 +0100

    Db commect

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+const connectDB = async () =>{
+  const mongouri = process.env.MONGO_URI;
+  if(!mongouri){
+    throw new Error("No mongodb uri")
+  }
+  try {
+    const connection = await  mongoose.connect(mongouri,{
+       useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
