dc71b31b4d251793f47f6f9e8292857e7f92f6cb
Ghost <culestfrosh@gmail.com>
2023-03-18 15:02:06 +0100

    users routing

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const {
+  authUser,
+  createUser,
+  delUser,
+  editUser,
+  users,
+  user,
+  followUser
+} = require("../controllers/user")
+const {
+  protect,
+  protectMe
+ } = require("../middlewares/auth")
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/',protect,users);
+//create a new user
+router.post('/',createUser);
+// User login
+router.post('/user/login',authUser);
+// Get a user by id
+router.get('/user/:id',protect,user);
+// Update a users information 
+router.put('/user/:id',protect,protectMe,editUser);
+// delete a user's account 
+router.delete('/user/:id',protect,protectMe,deluser);
+// follow user
+router.post("/user/:id/follow",protect,followUser)
 
 module.exports = router;
