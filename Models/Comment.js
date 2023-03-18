af7acc87c0ba0ae3bd69b3fede9686eeb7cddf28
Ghost <culestfrosh@gmail.com>
2023-03-18 12:47:27 +0100

    A comment model

---
+++ Models/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+
+const CommentSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Comment = mongoose.model("Comment",CommentSchema)
