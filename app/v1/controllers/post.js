53e04708bf77913e32f0afde4699c9c69888ff23
Ghost <culestfrosh@gmail.com>
2023-05-20 08:35:23 +0100

    updated to post controller

--- app/v1/controllers/post.js
+++ app/v1/controllers/post.js
@@ -3,8 +3,9 @@ const Post = require("../models/Post")
 
 
 const createPost = async (req,res,next)=>{
-  const dPost = post.createPost(req.body)
+  const dPost = await post.createPost(req.body)
   if(typeof dPost === "object"){
+    console.log(dPost)
     res.status(200).json({post:dPost})
 
   }else{
@@ -13,7 +14,7 @@ const createPost = async (req,res,next)=>{
 }
 const editPost = async (req,res)=>{
   const { body } = req;
-  const dPost = Post.findById(id);
+  const dPost = await Post.findById(id);
   const user = req.user;
   if (dPost) {
         dPost[key] = body[key] ? body[key] : dPost[key]
@@ -24,18 +25,19 @@ const editPost = async (req,res)=>{
   }
 }
 const commentPost = async (req,res)=>{
-  const { id,comment,post } = req.body;
-  const dPost = Post.findById(id);
+  const { comment } = req.body;
+  const { id } = req.params
+  const dPost = await Post.findById(id);
   const user = req.user;
   if (dPost) {
-    dPost.comment.push({
-      author:user,
+    dPost.comments.push({
+      author:user._id,
       likes:{
-        author:user,
-        post:post._id
+        author:user._id,
+        post:id
       },
       content:comment,
-      post:post._id
+      post:id
     })
     await dPost.save()
     res.status(200).json({post:dPost})
@@ -44,13 +46,13 @@ const commentPost = async (req,res)=>{
   }
 }
 const likePost = async (req,res)=>{
-  const { id,post } = req.body;
-  const dPost = Post.findById(id);
+  const { id } = req.params;
+  const dPost = await Post.findById(id);
   const user = req.user;
   if (dPost) {
     dPost.likes.push({
-        author:user,
-        post:post._id
+        author:user._id,
+        post:id
     })
     await dPost.save()
     res.status(200).json({post:dPost})
@@ -59,7 +61,7 @@ const likePost = async (req,res)=>{
   }
 }
 const userPost = async (req,res)=>{
-  const dPost = post.getAllPostByUser(req.body.id)
+  const dPost = await post.getAllPostByUser(req.body.id)
   if(typeof dPost === "object"){
     res.status(200).json({post:dPost})
 
@@ -68,7 +70,7 @@ const userPost = async (req,res)=>{
   }
 }
 const posts = async (req,res)=>{
-  const dPosts = post.posts();
+  const dPosts = await post.posts();
   if(typeof dPosts === "object"){
     res.status(200).json({posts:dPosts})
 
@@ -77,7 +79,7 @@ const posts = async (req,res)=>{
   }
 }
 const posT = async (req,res,next)=>{
-  const dPost = post.getPostById(req.params.id);
+  const dPost = await post.getPostById(req.params.id);
   if(typeof dPost === "object"){
     res.status(200).json({post:dPost})
 
@@ -86,9 +88,9 @@ const posT = async (req,res,next)=>{
   }
 }
 const delPost = async (req,res,next)=>{
-  const dPost = post.delPost(req.params.id);
+  const dPost = await post.delPost(req.params.id);
   if(dPost){
-    res.status(200).json({posts:dPosts})
+    res.status(200).json({posts:dPost})
 
   }else{
     res.status(400).json({error: dPosts})
