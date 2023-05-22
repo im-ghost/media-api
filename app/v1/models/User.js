f66b783ac5b8fdc5ba8ecf19ab832edcd433f52c
Ghost <culestfrosh@gmail.com>
2023-05-22 15:05:35 +0100

    used a presave function to automatically hash password Incase of update

--- app/v1/models/User.js
+++ app/v1/models/User.js
@@ -28,7 +28,7 @@ const UserSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'User',
   }],
-  post: [{
+  posts: [{
     type: Schema.Types.ObjectId,
     ref: 'Post',
   }],
@@ -37,6 +37,14 @@ const UserSchema = new Schema({
     required:true
   }
 });
+UserSchema.pre("save", async function (next)=>{
+  if(!this.isModified()){
+    next()
+  }
+  const salt = await bcrypt.genSalt(12)
+  this.password = await bcrypt.hash(this.password,salt)
+  next()
+})
 UserSchema.methods.matchPassword = async function (pass){
   if(await bcrypt.compare(pass,this.password)){
     return true
