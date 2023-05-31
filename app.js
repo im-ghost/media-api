c985ae355bde77371577a89852bef69626e0b37c
Ghost <culestfrosh@gmail.com>
2023-05-31 21:39:10 +0100

    socket.io

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const { Server } = require("socket.io");
+const http = require("http");
 const indexRouter = require('./app/v1/routes/index');
 const usersRouter = require('./app/v1/routes/users');
 const postRouter = require('./app/v1/routes/posts');
 require("dotenv").config()
 const connectDB = require("./app/v1/config/db");
+const { initializeSocket } = require("./app/v1/utils/socket");
 connectDB()
 const cors = require("cors")
 const app = express();
-app.use(cors(/*{
-  origin:["localhost:3000"]
-  }*/))
+const server = http.createServer(app)
+
+initializeSocket(server,Server)
+app.use(cors({
+  origin:["http://localhost:3000"]
+  }))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use('/api/v1/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/posts', postRouter);
@@ -47,5 +51,8 @@ app.use((err, req, res, next)=>{
 const port = process.env.PORT || 5000;
 app.listen(port,()=>{
   console.log(`Running on port : ${port}`)
+});
+server.listen(5000,()=>{
+  console.log("Running on port 5000")
 })
 module.exports = app;
