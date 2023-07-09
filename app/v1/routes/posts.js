5f855051ee91e6887922d9cb3a2d46d9344522d9
Richard <culestfrosh@gmail.com>
2023-07-10 00:26:45 +0100

    unretweet and unlike

--- app/v1/routes/posts.js
+++ app/v1/routes/posts.js
@@ -10,7 +10,9 @@ const {
   userPost,
   commentPost,
   likePost,
-  retweetPost
+  retweetPost,
+  unlikePost,
+  unretweetPost
 } = require("../controllers/post")
 const {
   protect,
@@ -18,7 +20,7 @@ const {
   protectPost
  } = require("../middlewares/auth")
 /* GET posts listing. */
-router.route("/").get(protect,posts).post(createPost);
+router.route("/").get(protect,posts).post(protect,createPost);
 // Get a user's post listings 
 router.post('/post/user/:id',userPost);
 // Get a post by id
@@ -30,8 +32,10 @@ router.put("/post/like/:id/",protect,likePost)
 // comment on a post 
 router.put("/post/comment/:id/",protect,commentPost)
 router.put("/post/like/:id/",protect,likePost)
+router.put("/post/unlike/:id/",protect,unlikePost)
 // retweet on a post 
 router.put("/post/retweet/:id/",protect,retweetPost)
+router.put("/post/unretweet/:id/",protect,unretweetPost)
 router.delete('/post/:id/:author',protect,protectPost,delPost);
 
 router.put('/post/:id/:author',protect,protectPost,editPost);
