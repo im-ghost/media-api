5d61794ecd5072a3b2f429c89ab41307c674b075
Richard <culestfrosh@gmail.com>
2023-07-14 09:10:21 +0100

    notification model

---
+++ app/v1/models/Notification.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+
+const NotificationSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  date:{
+    type:Date,
+    default:()=> Date.now,
+  }
+});
+
+const Notification = mongoose.model("Notification",NotificationSchema)
+
+module.exports = Notification;
