f4a2ecf6834c6017e884de10db5000e97571d6cd
Richard <culestfrosh@gmail.com>
2023-07-15 23:46:07 +0100

    updated users routes

--- app/v1/routes/users.js
+++ app/v1/routes/users.js
@@ -6,7 +6,7 @@ const {
   delUser,
   editUser,
   users,
-  useR:user,
+  useR: user,
   followUser,
   unfollowUser,
   logOutUser,
@@ -14,26 +14,36 @@ const {
   getNotifications,
   postNotification,
   deleteNotification
-} = require("../controllers/user")
+} = require("../controllers/user");
 const {
   protect,
   protectMe,
   protectNotification
- } = require("../middlewares/auth")/*
- const multer = require("multer")
- const upload = multer({ destination :"upload/"});*/
-/* GET users listing. */
-router.route('/').get(protect,users).post(/*upload.single("image")*/createUser);
+} = require("../middlewares/auth");
+
 // User login
-router.post('/login',authUser);
-router.post('/ologin',oauthLogin);
-router.post('/logout',logOutUser);
-// Get a user by id
-router.route('/user/:id').get(user).put(protect,protectMe,editUser).delete(protect,protectMe,delUser);
-router.route('/user/:id/notifications').get(protect,getNotifications).post(protect,postNotification)
-router.delete("/user/:id/notifications/notificationId",protect,protectNotification,deleteNotification);
-// follow user
-router.post("/user/:id/follow",protect,followUser)
-router.post("/user/:id/unfollow",protect,unfollowUser)
+router.post('/login', authUser);
+router.post('/ologin', oauthLogin);
+router.post('/logout', logOutUser);
+
+// Get notifications for a user
+router.route('/user/notifications/:id')
+  .get(protect, getNotifications)
+  .post(protect, postNotification);
+
+router.delete("/user/:id/notifications/:notificationId", protect, protectNotification, deleteNotification);
+
+// Follow/unfollow user
+router.post("/user/:id/follow", protect, followUser);
+router.post("/user/:id/unfollow", protect, unfollowUser);
+
+// Get all users or create a new user
+router.route('/').get(protect, users).post(createUser);
+
+// Get a user by id, edit, or delete a user
+router.route('/user/:id')
+  .get(user)
+  .put(protect, protectMe, editUser)
+  .delete(protect, protectMe, delUser);
 
 module.exports = router;
