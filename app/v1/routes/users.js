62d7f151cc637d56a0332240403594bbdb990597
Richard <culestfrosh@gmail.com>
2023-07-15 09:15:56 +0100

    updated delete comment

--- app/v1/routes/users.js
+++ app/v1/routes/users.js
@@ -30,7 +30,8 @@ router.post('/ologin',oauthLogin);
 router.post('/logout',logOutUser);
 // Get a user by id
 router.route('/user/:id').get(user).put(protect,protectMe,editUser).delete(protect,protectMe,delUser);
-router.route('/user/:id/notifications').get(protect,getNotifications).post(protect,postNotification).delete(protect,protectNotification,deleteNotification);
+router.route('/user/:id/notifications').get(protect,getNotifications).post(protect,postNotification)
+route.delete("/user/:id/notifications/notificationId",protect,protectNotification,deleteNotification);
 // follow user
 router.post("/user/:id/follow",protect,followUser)
 router.post("/user/:id/unfollow",protect,unfollowUser)
