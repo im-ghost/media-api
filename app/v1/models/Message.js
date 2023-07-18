78dcf3ceedabe4919b5f78bdbe55c3ada0693dc3
Richard <culestfrosh@gmail.com>
2023-07-18 20:51:01 +0100

    read

--- app/v1/models/Message.js
+++ app/v1/models/Message.js
@@ -19,6 +19,10 @@ const messageSchema = new Schema({
     type: Date,
     default: Date.now,
   },
+  read:{
+    type:Boolean,
+    default:false
+  }
 });
 
 const Message = model("Message",messageSchema)
